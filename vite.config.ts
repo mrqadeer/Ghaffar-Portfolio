0f25d44143d6270c3086ef21092690aaa2bd36aa
mrqadeer <mrqadeer1231122@gmail.com>
2024-10-14 22:39:41 +0500

    deployment stuff added

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://mrqadeer.github.io/Ghaffar-Portfolio/',
 });
