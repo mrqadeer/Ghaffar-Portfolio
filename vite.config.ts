f33b2ecb2bf25cf29ede5c37c2b4fddc69927742
mrqadeer <mrqadeer1231122@gmail.com>
2024-10-14 22:46:07 +0500

    updated vite configration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://mrqadeer.github.io/Ghaffar-Portfolio/',
+  base: '/Ghaffar-Portfolio/',  // Only the repository name, with slashes
 });
